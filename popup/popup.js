42e356bbc46de9899d8f30cf85a8b1897484da66
MDTsai <md.tsai@gmail.com>
2017-10-06 14:50:47 +0800

    Fixes #4 - Add common bugs
    Add common bugs from https://wiki.mozilla.org/Compatibility/CommonIssues
    Load commonbugs.json from https://mdtsai.github.io/commonbugs.json

--- popup/popup.js
+++ popup/popup.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var products = null;
+var commonbugs = null;
 
 document.querySelector("#product").addEventListener("change", (e) => {
     // Clear current components options
@@ -26,6 +27,11 @@ document.querySelector("#submit").addEventListener("click", (e) => {
             product: document.querySelector("#product").value,
             component: document.querySelector("#component").value
         });
+    } else if (document.querySelector("#commonbug").checked) {
+        var sending = browser.runtime.sendMessage({
+            type: "seealso",
+            bugnumber: document.querySelector("#commonbugs").value
+        });
     } else {
         var sending = browser.runtime.sendMessage({
             type: "seealso",
@@ -38,17 +44,27 @@ document.querySelector("#submit").addEventListener("click", (e) => {
 });
 
 function handleResponse(message) {
-    products = JSON.parse(message.response);
+    products = JSON.parse(message.products);
     var elProducts = document.querySelector("#product");
     elProducts.innerHTML = "";
     for (var product in products) {
-        var new_product = document.createElement('option');
+        var new_product = document.createElement("option");
         new_product.value = product;
         new_product.innerHTML = product;
         elProducts.appendChild(new_product);
     }
     // unselect to make sure we have product/component updated
     elProducts.selectedIndex = -1;
+
+    commonbugs = JSON.parse(message.commonbugs);
+    var elCommonBugs = document.querySelector("#commonbugs");
+    elCommonBugs.innerHTML = "";
+    for (var bug in commonbugs) {
+        var new_bug = document.createElement("option");
+        new_bug.value = bug;
+        new_bug.innerHTML = commonbugs[bug];
+        elCommonBugs.appendChild(new_bug);
+    }
 }
 
 window.addEventListener("load", (e) => {
